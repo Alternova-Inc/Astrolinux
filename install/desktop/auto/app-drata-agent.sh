67b37f618ea1b80aac1d6f3a854a9f5032c57a14
EdwinFVilla <edwin.villa@alternova.com>
2024-09-27 08:38:27 -0500

    ✨ chore(app-drata-agent.sh) - Add gum-loader and improve desktop file modification checks for Drata Agent installation stability

--- install/desktop/auto/app-drata-agent.sh
+++ install/desktop/auto/app-drata-agent.sh
@@ -2,6 +2,7 @@
 set -e
 
 source ~/.local/share/astrolinux/gum/gum-styles.sh
+source ~/.local/share/astrolinux/gum/gum-loader.sh
 
 gum_styled_text "Installing drata agent..."
 
@@ -13,17 +14,16 @@ LATEST_RELEASE=$(curl --silent "https://api.github.com/repos/drata/agent-release
 
 DOWNLOAD_URL="https://github.com/drata/agent-releases/releases/download/$LATEST_RELEASE/$ASSET"
 
-wget -qO $ASSET "$DOWNLOAD_URL"
+wget -qO "$ASSET" "$DOWNLOAD_URL"
 
-sudo dpkg -i ./$ASSET
-sudo apt-get install -f
+sudo dpkg -i "./$ASSET"
 
-rm $ASSET
+rm "$ASSET"
 
 cd -
 
 # There is a problem in the manual executable and automatic execution of Drata Agent in Ubuntu version 24.04, for this the following solution is generated:
-
+# issue https://github.com/drata/drata-agent/issues/20
 # Make sure that the executable file has execution permissions.
 EXECUTABLE_PATH="/opt/Drata Agent/drata-agent"
 sudo chmod +x "$EXECUTABLE_PATH"
@@ -32,16 +32,25 @@ sudo chmod +x "$EXECUTABLE_PATH"
 modify_desktop_file() {
   local file_path="$1"
   if [ -f "$file_path" ]; then
-    sudo sed -i 's|Exec=.*|Exec="'"$EXECUTABLE_PATH"'" --no-sandbox %U|' "$file_path"
-    gum_styled_text "Modified $file_path to include --no-sandbox."
+    if grep -q -- "--no-sandbox" "$file_path"; then
+      gum_styled_text "No need to modify $file_path, --no-sandbox already present."
+    else
+      sudo sed -i 's|Exec=.*|Exec="'"$EXECUTABLE_PATH"'" --no-sandbox %U|' "$file_path"
+      gum_styled_text "Modified $file_path to include --no-sandbox."
+    fi
   else
-    gum_styled_text "The file $file_path was not found."
+    gum_styled_text "The file $file_path was not found. Aborting modification process."
+    exit 1
   fi
 }
 
 # Modify the .desktop files
 modify_desktop_file "/usr/share/applications/drata-agent.desktop"
-modify_desktop_file "$HOME/.config/autostart/drata-agent.desktop"
+gtk-launch drata-agent
+gum_loader "Starting Drata Agent..." 5
+
+la linea siguiente esta funcionando pero al reiniciar el sistema del archivo se elimnina el --no-sandbox %U
+modify_desktop_file "/home/$USER/.config/autostart/drata-agent.desktop"
 
 # Verify that the executable file is executable and exists
 if [ -x "$EXECUTABLE_PATH" ]; then
