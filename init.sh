dec80816de4bea7f6fdefa7ddd7a9657cde18f39
EdwinFVilla <edwin.villa@alternova.com>
2024-10-04 18:55:27 -0500

    🐛 fix/frozen terminal:
    The terminal continues to freeze and does not let you interact with the options when installing vscode.

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ is_gnome_terminal_available() {
 
 # Check if gnome-terminal is available
 if is_gnome_terminal_available; then
-    gnome-terminal --geometry=90x24 -- bash -c "wget -qO- https://raw.githubusercontent.com/Alternova-Inc/Astrolinux/main/boot.sh | bash; exec bash"
+    wget -qO- https://raw.githubusercontent.com/Alternova-Inc/Astrolinux/develop/boot.sh | bash; exec bash
 else
     echo "gnome-terminal is not available. Open this script in Ubuntu 24.04 with gnome-terminal."
 fi
