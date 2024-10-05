81918b4dd49f921a71483e1aba59ad1ee1e7bd1d
EdwinFVilla <edwin.villa@alternova.com>
2024-10-04 19:22:10 -0500

    🐛 fix/Init.sh:
    Improved boot.sh call and integrated row and column resizing of the terminal to improve the display of the content.

--- init.sh
+++ init.sh
@@ -6,9 +6,15 @@ is_gnome_terminal_available() {
     command -v gnome-terminal >/dev/null 2>&1
 }
 
+# Attempt to resize the current terminal
+resize_terminal() {
+    printf '\e[8;24;90t'  # Cambia el tamaño a 24 filas y 90 columnas
+}
+
 # Check if gnome-terminal is available
 if is_gnome_terminal_available; then
-    wget -qO- https://raw.githubusercontent.com/Alternova-Inc/Astrolinux/main/boot.sh | bash; exec bash
+    resize_terminal  # Redimensionar la terminal actual
+    eval "$(wget -qO- https://raw.githubusercontent.com/Alternova-Inc/Astrolinux/main/boot.sh)"
 else
     echo "gnome-terminal is not available. Open this script in Ubuntu 24.04 with gnome-terminal."
 fi
